863d842e07eb57e90d3350c24167dd118f62f7c7
sgardner01 <s.gardner01@outlook.com>
2022-01-13 23:07:30 -0700

    double first

---
+++ code.js
@@ -0,0 +1,77 @@
+console.log("wow, thanos")
+
+//DATA TYPES
+
+
+
+
+
+
+
+
+
+// string
+// "Hello World"
+
+
+
+
+
+
+
+
+
+
+// Boolean
+// true//false
+
+
+
+
+
+
+
+
+
+
+
+// Number
+// 1.233123
+// -40
+
+// Integers
+// -4
+// -2
+// 0
+// 599
+
+
+
+
+// array
+// ["cat", 'dog', 'mouse']
+
+
+
+
+
+// object
+// let avengers = {heroName: "spiderman", person:"peter", moto: "with great power comes greatr responsibilty"}
+
+
+
+
+//COMMON CODE PATERNS
+// If Block
+if (true){
+    console.log("again, spiderman is good")
+}
+
+let movie = 'good'
+// If-Else Blco
+if (movie === "good"){
+    console.log("watch it again")
+
+} else {
+    console.log("Get your money back")
+}
